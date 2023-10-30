7089b0057582d4bb34d20bb123229081e42283ec
Sebastian Aristizabal Cast <sebastian.aristizabalc@udea.edu.co>
2023-10-29 22:48:19 -0500

    juego 3 en linea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import ActualizarPanta from '@/components/ActualizarPanta'
 import ActualizarPanta2 from '@/components/ActualizarPanta2'
+import Game from '@/components/App3Lineas'
+import Board from '@/components/App3Lineas'
+import Juego from '@/components/App3Lineas'
 import Boton from '@/components/Componente'
 import Perfil from '@/components/MostrarDatos'
 import RenderCondicional from '@/components/RenderCondicional'
@@ -19,6 +22,10 @@ const Index = () => {
       <ResponderEvent></ResponderEvent>
       <ActualizarPanta></ActualizarPanta>
       <ActualizarPanta2></ActualizarPanta2>
+      <div>
+        <h2>Juego 3 en linea</h2>
+        <Game></Game>
+      </div>
     </div>
   )
 }
